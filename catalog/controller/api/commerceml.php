d481d8113d3f73801e872d3e981d1fb136c42197
svtol_iMac <seledok@gmail.com>
2019-11-22 09:22:02 +0700

    commerceml

---
+++ catalog/controller/api/commerceml.php
@@ -0,0 +1,381 @@
+<?php
+// *	@copyright	OPENCART.PRO 2011 - 2015.
+// *	@forum	http://forum.opencart.pro
+// *	@source		See SOURCE.txt for source and other copyright.
+// *	@license	GNU General Public License version 3; see LICENSE.txt
+
+class ControllerApiCommerceml extends Controller {
+
+    static $vendor_id = 0;
+    private $order_list = array();
+
+    private $login = 'alex';
+    private $password = 'asdvrxe3543';
+
+    public function index() {
+
+        if(isset($_SERVER['PHP_AUTH_USER'])) {
+             $username = $_SERVER['PHP_AUTH_USER'];
+             $password = $_SERVER['PHP_AUTH_PW'];
+        } else {
+            if(isset($_GET['user'])) {
+                $username = $_GET['user'];
+                $password = $_GET['pass'];
+            } else {
+                header("HTTP/1.0 401 Unauthorized");
+                exit;
+            }
+        }
+
+
+        if($username != $this->login || $password !=  $this->password){
+            header("HTTP/1.0 403 Forbidden");
+            exit;
+        }
+
+
+        $mode = $_GET['type'].'_'.$_GET['mode'];
+
+        call_user_func(array($this,$mode));
+
+        exit;
+    }
+
+    private function catalog_checkauth() {
+
+        die ('success
+exchsession
+'.time());
+    }
+
+    private function catalog_init() {
+        die ('zip=yes
+file_limit=2000000');
+    }
+
+    private function PostPrices()
+    {
+        $id = $_COOKIE['exchsession'];
+        ob_start();
+        $filename = $_GET['filename'];
+        $data = file_get_contents('php://input');
+        $path = DIR_UPLOAD .$filename;
+        $handle = fopen($path, 'a');
+        fwrite($handle, $data);
+        fclose($handle);
+        $errors = ob_get_contents();
+        ob_end_clean();
+        die('success');
+    }
+
+    private function catalog_file() {
+        $id = $_COOKIE['exchsession'];
+        ob_start();
+        $filename = $_GET['filename'];
+        $data = file_get_contents('php://input');
+        $path = DIR_UPLOAD .$filename;
+        $handle = fopen($path, 'a');
+        fwrite($handle, $data);
+        fclose($handle);
+        $errors = ob_get_contents();
+        ob_end_clean();
+        die('success');
+    }
+
+    private function catalog_import() {
+        die('success');
+
+    }
+
+
+    private function sale_checkauth()
+    {
+        die ('success
+exchsession
+'.time());
+    }
+
+    private function sale_init()
+    {
+        die ('zip=yes
+file_limit=2000000');
+    }
+
+    private function sale_query()
+    {
+        //die('success');
+        echo $this->make_order_xml();
+    }
+
+    private function sale_success()
+    {
+
+        die('success');
+    }
+
+    private function sale_file()
+    {
+        $id = $_COOKIE['exchsession'];
+        ob_start();
+        $filename = $_GET['filename'];
+        $data = file_get_contents('php://input');
+        $path = '/var/www/gotti/data/sync/vendors/'.self::$vendor_id.'/in/'.$filename;
+        $handle = fopen($path, 'a');
+        fwrite($handle, $data);
+        fclose($handle);
+        $errors = ob_get_contents();
+        ob_end_clean();
+        die('success');
+    }
+
+    private function get_vendor_orders($vendor_id)
+    {
+        return  $this->db->get_rows("SELECT * FROM gotti.1c_order_log WHERE order_status_id = '5' && replic_id = '0' && vendor_id = '$vendor_id'");
+    }
+
+     function make_order_xml()
+    {
+
+
+        // для тестов
+        if(!self::$vendor_id) self::$vendor_id = 7;
+
+
+        $base = $this->base;
+        $sync = new product_sync($this->registry);
+        $orders = $this->get_vendor_orders(self::$vendor_id);
+
+        $dom = new domDocument("1.0", "windows-1251");
+        //$dom = new domDocument("1.0", "utf-8");
+        $dom->formatOutput = true;
+        $root = $dom->createElement("КоммерческаяИнформация");
+        $root->setAttribute("ВерсияСхемы", '2.07');
+        $root->setAttribute("ДатаФормирования", date('Y-m-dТH:m:s'));
+        $dom->appendChild($root);
+
+
+        if($orders) {
+
+            $this->db->query("INSERT INTO gotti.1c_replic_out SET filename = 'order.xml' ");
+            $replic_id = $this->db->getLastId();
+
+            foreach ($orders as $order) {
+                // проверка что заказ был в статусе 5 // передан на склад
+                $is_work = $this->db->query("SELECT * FROM " . $order['shop'] . ".oc_order_history WHERE order_id = '" . $order['order_id'] . "' && order_status_id =  '5' ");
+
+                if (!$is_work->num_rows && $order['order_status_id'] != 5) {
+                    echo 'Note: Заказ ' . $order['order_id'] . " из магазина " . $order['shop'] . " не выгружкен - его не пердавали на склад\n\r";
+                    continue;
+                }
+                // проверка что товар не локальный будет ниже
+
+                // что делать с заказами где локальный и не локальный товар?
+
+                /** если заказ из оптового магазина то возьмеме ИНН покупателя
+                 *   если заказ от пратенра то используем его ИНН
+                 */
+                if ($base->shops[$order['shop']]['tariff'] > 4) {
+                    $order_customer_info = $this->db->get_row("SELECT * FROM " . $order['shop'] . ".oc_order as o 
+                LEFT JOIN " . $order['shop'] . ".oc_customer as c ON c.customer_id = o.customer_id 
+                LEFT JOIN " . $order['shop'] . ".oc_address as a ON a.customer_id = o.customer_id 
+                WHERE o.order_id = '" . $order['order_id'] . "' ");
+
+                    // var_dump($order_customer_info);
+                    // если это база готти получим INN
+
+                    if (!$order_customer_info['fax'] || !stat::is_valid_inn($order_customer_info['fax'])) {
+                        //echo 'Note: Заказ ' . $order['order_id'] . " из магазина " . $order['shop'] . " не выгружкен - он сделан без регистрации или ИНН указан неверно\n\r";
+                        $not[] = $order['global_order_id'];
+                        continue;
+                    } else {
+                        $inn = $order_customer_info['fax'];
+                        $org_name = addslashes($order_customer_info['company']);
+
+                    }
+
+                } else {
+                    $inn = $base->shops[$order['shop']]['inn'];
+                    $set = $sync->getSetting('config', $order['shop']);
+                    $org_name = $set['config_owner'];
+                    if (!$inn) {
+                        echo 'Note: Заказ ' . $order['order_id'] . " из магазина " . $order['shop'] . " не выгружкен - нет ИНН партнера\n\r";
+                        $not[] = $order['global_order_id'];
+                        continue;
+                    }
+                }
+
+                $summ = 0;
+                $products = $this->db->query("SELECT op.order_id, o.date_added, vp.guid as model, p.sku, op.quantity, op.price_in as price, pd.name, o.store_name, o.shipping_zone 
+    FROM " . $order['shop'] . ".oc_order_product op
+    LEFT JOIN " . $order['shop'] . ".oc_order o ON o.order_id = op.order_id
+    LEFT JOIN gotti.oc_product p ON p.product_id = op.product_id
+    LEFT JOIN gotti.1c_vendor_product vp ON vp.vendor_id = " . self::$vendor_id . " && vp.product_id = p.product_id
+    LEFT JOIN gotti.oc_product_description pd ON pd.product_id = op.product_id
+     
+    WHERE op.order_id = '" . $order['order_id'] . "' && p.product_id IS NOT NULL");
+
+                // echo 'Выгружен заказ № '.$order['order_id'].' из базы '.$order['shop'].' ИНН:'.$inn."\n\r";
+
+
+                if ($products->num_rows == 0) continue;
+
+
+                foreach ($products->rows as $item) {
+                    $summ += $item['price'] * $item['quantity'];
+                }
+                unset($doc);
+                unset($docdata);
+                $doc = $dom->createElement("Документ");
+                $docdata[] = $dom->createElement("Ид", 'itr-order-' . $order['global_order_id']);
+                $docdata[] = $dom->createElement("Номер", $order['global_order_id']);
+                $docdata[] = $dom->createElement("ХозОперация", 'Заказ товара');
+                $docdata[] = $dom->createElement("Роль", 'Продавец');
+                $docdata[] = $dom->createElement("Валюта", 'Руб');
+                $docdata[] = $dom->createElement("Дата", date('Y-m-d', strtotime($order['date_added'])));
+                $docdata[] = $dom->createElement("Курс", 1);
+                $docdata[] = $dom->createElement("Сумма", $summ);
+                // $docdata[] = $dom->createElement("org_inn", '541014835226');
+
+
+                $docdata['users'] = $dom->createElement("Контрагенты");
+                unset($user);
+                unset($userdata);
+                $user = $dom->createElement("Контрагент");
+
+                $userdata[] = $dom->createElement("Наименование", $org_name);
+                $userdata[] = $dom->createElement("ОфициальноеНаименование", $org_name);
+
+                $userdata[] = $dom->createElement("ИНН", $inn);
+                $userdata[] = $dom->createElement("Роль", 'Покупатель');
+
+
+                foreach ($userdata as $item) {
+                    $user->appendChild($item);
+                }
+                $docdata['users']->appendChild($user);
+
+
+                $zone = ' /' . $products->row['shipping_zone'] . '/';
+                $gorod = isset($sync->shops[$order['shop']]['gorod']) ? $sync->shops[$order['shop']]['gorod'] . $zone : 'Новосибирск-ОПТ';
+
+                $docdata[] = $dom->createElement("Комментарий", $gorod . ' - ' . addslashes($products->row['store_name']) . ' / ');
+
+                $docdata['taxes'] = $dom->createElement("Налоги");
+                unset($tax);
+                unset($taxdata);
+                $tax = $dom->createElement("Налог");
+                $taxdata[] = $dom->createElement('Наименование', "НДС");
+                $taxdata[] = $dom->createElement("УчтеноВСумме", 'true');
+                $taxdata[] = $dom->createElement("Сумма", round($summ * 0.18, 2));
+                foreach ($taxdata as $item) {
+                    $tax->appendChild($item);
+                }
+                $docdata['taxes']->appendChild($tax);
+
+
+                $docdata['products'] = $dom->createElement("Товары");
+                foreach ($products->rows as $product) {
+                    unset($oneproduct);
+                    unset($oneproductdata);
+                    $oneproduct = $dom->createElement("Товар");
+                    $oneproductdata[] = $dom->createElement("Ид", $product['model']);
+                    $oneproductdata[] = $dom->createElement("Артикул", $product['sku']);
+                    $oneproductdata[] = $dom->createElement("Наименование", htmlspecialchars($product['name']));
+                    $oneproductdata[] = $dom->createElement("Количество", $product['quantity']);
+                    $oneproductdata[] = $dom->createElement("ЦенаЗаЕдиницу", $product['price']);
+                    $oneproductdata[] = $dom->createElement("Сумма", $product['price'] * $product['quantity']);
+
+                    unset($tax);
+                    unset($taxdata);
+                    $oneproductdata['taxes'] = $dom->createElement("Налоги");
+                    $tax = $dom->createElement("Налог");
+                    $taxdata[] = $dom->createElement('Наименование', "НДС");
+                    $taxdata[] = $dom->createElement("УчтеноВСумме", 'true');
+                    $taxdata[] = $dom->createElement("Сумма", round($product['price'] * $product['quantity'] * 0.18, 2));
+                    foreach ($taxdata as $item) {
+                        $tax->appendChild($item);
+                    }
+                    $oneproductdata['taxes']->appendChild($tax);
+
+                    foreach ($oneproductdata as $item) {
+                        $oneproduct->appendChild($item);
+                    }
+                    $docdata['products']->appendChild($oneproduct);
+
+                }
+                unset($status);
+                unset($status_name);
+                unset($status_zn);
+                unset($sait);
+                unset($sait_zn);
+                $docdata['r'] = $dom->createElement("ЗначенияРеквизитов");
+                $status = $dom->createElement("ЗначениеРеквизита");
+                $status_name = $dom->createElement("Наименование", 'СтатусДокумента');
+                $status_zn = $dom->createElement("Значение", $order['order_status_id']);
+
+                $sait = $dom->createElement("ЗначениеРеквизита");
+                $sait_name = $dom->createElement("Наименование", 'Сайт');
+                $sait_zn = $dom->createElement("Значение", '[s1] ГОТТИ Регион');
+
+                $otm = $dom->createElement("ЗначениеРеквизита");
+                $otm_name = $dom->createElement("Наименование", 'Отменен');
+                $otm_zn = $dom->createElement("Значение", 'false');
+
+                $otm->appendChild($status_name);
+                $otm->appendChild($status_zn);
+
+                $status->appendChild($status_name);
+                $status->appendChild($status_zn);
+
+                $sait->appendChild($sait_name);
+                $sait->appendChild($sait_zn);
+                $sait->appendChild($otm);
+
+                $docdata['r']->appendChild($status);
+                $docdata['r']->appendChild($sait);
+
+
+                foreach ($docdata as $item) {
+                    $doc->appendChild($item);
+                }
+
+                $root->appendChild($doc);
+
+                $orders_in_replic[$order['shop']][] = $order['global_order_id'];
+            }
+
+
+
+            //
+            foreach ($orders as $order) {
+                // если заказ не надо выгружать
+                if(isset($not) && in_array($order['global_order_id'],$not))
+                    $this->db->query("UPDATE gotti.1c_order_log SET replic_id = NULL WHERE order_id = '" . $order['order_id'] . "' && shop = '".$order['shop']."' ");
+                else {
+                    // ид реплики
+                    $this->db->query("UPDATE gotti.1c_order_log SET replic_id = '$replic_id' WHERE global_order_id = " .  $order['global_order_id'] . " ");
+                    // статус заказа
+                    $this->db->query("UPDATE ".$order['shop'].".oc_order SET order_status_id = '7' WHERE order_id = ".$order['order_id']." ");
+                    // история
+                    $this->db->query("INSERT INTO ".$order['shop'].".oc_order_history SET order_status_id = '7', order_id = ".$order['order_id'].", `comment` = 'Заказ был передан в учетную систему склада', date_added = NOW()  ");
+                }
+
+            }
+
+
+            //$orders = implode(',',$orders_in_replic);
+            //$this->db->query("UPDATE gotti.1c_order_log SET replic_id = '$replic_id' WHERE global_order_id IN (" . $orders . ") ");
+            /** todo: смнить статус заказа на Принят в работу! */
+            //
+        }
+            //echo $dom->saveXML();
+            return $dom->saveXML();
+
+
+    }
+
+    function order()
+    {
+        echo $this->make_order_xml();
+    }
+}
---
+++ system/library/importxml.php
@@ -0,0 +1,282 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: seledok
+ * Date: 07.12.16
+ * Time: 15:00
+ */
+
+class importXml
+{
+    protected $manufact;
+    public $add_cnt = 0;
+    public $skip_cnt = 0;
+    public $price_cnt = 0;
+    public $upd_cnt = 0;
+    public $null_cnt = 0;
+    public $params = array();
+    private $category_array = array();
+    public $status = '';
+    public $type = 0;
+    public $import_time;
+    protected $parent_cat=array(0,0,0,0,0,0,0);
+
+
+
+    public function __construct($registry)
+    {
+
+
+    }
+
+    public function parseXml($path,$vendor_id = 3)
+    {
+
+        $this->vendor_id = $vendor_id;
+        $reader = new XMLReader();
+        $reader->open($path);
+
+        $this->category_array = $this->shipper->get_categoryes_by_key($vendor_id);
+
+
+        while (@$reader->read()) {
+            if ($reader->nodeType == XMLReader::ELEMENT) {
+                //var_dump($reader->localName);
+                // если находим объект
+                if($reader->localName == 'КоммерческаяИнформация') $idate = $reader->getAttribute('ДатаФормирования');
+                if($reader->localName == 'Классификатор' || $reader->localName == 'ПакетПредложений') {
+                    if($reader->getAttribute('СодержитТолькоИзменения')=='false')
+                    {
+                        echo  date('H:m:s').' Загружаем полный '.$reader->localName.' от '.$idate. "\r\n";
+                        if($reader->localName == 'ПакетПредложений') $this->type = 1;
+                    }
+                    else
+                    {
+                        echo date('H:m:s').' Загружаем частичный '.$reader->localName.' от '.$idate. "\r\n";
+                        if($reader->localName == 'ПакетПредложений') $this->type = 0;
+                    }
+                }
+                elseif ($reader->localName == 'Товар') $this->xmlProduct($reader->readOuterXML());
+                elseif ($reader->localName == 'Предложение') $this->xmlOffer($reader->readOuterXML());
+                elseif ($reader->localName == 'Документ') $this->xmlOrder($reader->readOuterXML());
+
+            }
+
+        }
+
+        return $this->import_time;
+
+    }
+
+
+
+    public function xmlOrder($inner)
+    {
+
+        $xml = simplexml_load_string($inner);
+
+        $number = $xml->Номер->__toString();
+        $na = explode('-',$number);
+
+        if(count($na) < 2) return;
+        $data['shop'] = $na[0];
+        $data['order_id'] = (int)$na[1];
+
+        echo "Получен заказ ".$data['order_id']." в магазин ".$data['shop']."\n";
+
+        foreach ($xml->Товары->Товар as $product) {
+            $data['products'][$product->Ид->__toString()]['quantity'] = (int)$product->Количество->__toString();
+            $data['products'][$product->Ид->__toString()]['name'] = $product->Наименование->__toString();
+            $data['products'][$product->Ид->__toString()]['price'] = (float)$product->ЦенаЗаЕдиницу->__toString();
+        }
+
+
+        foreach ($xml->ЗначенияРеквизитов->ЗначениеРеквизита as $rekv) {
+            if ($rekv->Наименование->__toString() == 'Статуса заказа ИД')
+            {
+                $data['order_status_id'] = (int)$rekv->Значение->__toString();
+            }
+            if ($rekv->Наименование->__toString() == 'Номер по 1С')
+            {
+                $data['1c_number'] = trim($rekv->Значение->__toString());
+            }
+
+            if ($rekv->Наименование->__toString() == 'Отменен')
+            {
+                $data['order_info']['canceled'] = ($rekv->Значение->__toString()=='true') ? true : false;
+            }
+
+            if ($rekv->Наименование->__toString() == 'Проведен')
+            {
+                $data['proveden'] = $rekv->Значение->__toString() == 'false' ? false : true;
+            }
+            if ($rekv->Наименование->__toString() == 'ПометкаУдаления')
+            {
+                $data['deleted'] = $rekv->Значение->__toString() == 'false' ? false : true;
+            }
+            if ($rekv->Наименование->__toString() == 'Оплачен')
+            {
+                $data['payd'] = $rekv->Значение->__toString() == 'false' ? false : true;
+            }
+            if ($rekv->Наименование->__toString() == 'Отгружен')
+            {
+                $data['shiped'] = $rekv->Значение->__toString() == 'false' ? false : true;
+            }
+
+        }
+
+
+        // установим статус
+        if($data['order_status_id'] == 16 && !$data['shiped'] ) $data['order_status_id'] = 16;
+        elseif($data['deleted']) $data['order_status_id'] = 16;
+        elseif($data['shiped']) {
+            if($data['shop']=='opt_answare') {
+                $data['order_status_id'] = 20;
+            } else {
+                $data['order_status_id'] = 13;
+            }
+        }
+        elseif(!$data['proveden']) $data['order_status_id'] = 7;
+        elseif ($data['proveden']) {
+            if($data['order_status_id'] == 8) $data['order_status_id'] = 8;
+            elseif ($data['order_status_id'] == 9) { // ожидает оплаты
+                if($data['payd']) $data['order_status_id'] = 10;
+                else $data['order_status_id'] = 9;
+            }
+            elseif ($data['order_status_id'] == 11) $data['order_status_id'] = 11;
+        }
+
+
+        //var_dump($data);
+
+            // найдем выбранный заказ и  обновим
+            $order_sync = new order_sync($this->registry);
+            $order_sync->orderSync($data);
+
+
+    }
+
+    public function xmlProduct($inner)
+    {
+
+        $xml = simplexml_load_string($inner);
+        $vendor['vendor_id'] = $this->vendor_id; // ГОТТИ
+        $vendor['guid'] = $product['model']  = $xml->Ид->__toString();
+        $vendor['ean'] = $product['ean']  =$xml->Штрихкод->__toString();
+        $vendor['vendor_key'] = $product['ean']  =$xml->Код->__toString();
+        $vendor['name'] = $product['name'] = $xml->Наименование->__toString();
+        if(isset($xml->Группы->Ид)) $product['category'] = $xml->Группы->Ид->__toString();
+        if(isset($xml->Артикул)) $vendor['sku'] = $product['sku'] = $vendor['sku'] = $xml->Артикул->__toString();
+
+        if(isset($xml->Группы->Ид) && isset($this->category_array[$xml->Группы->Ид->__toString()])) $vendor['cat_id'] = $this->category_array[$xml->Группы->Ид->__toString()];
+
+        foreach ($xml->ЗначенияРеквизитов->ЗначениеРеквизита as $rekv) {
+            if ($rekv->Наименование->__toString() == 'Код') $vendor['vendor_key'] = $product['upc'] = $rekv->Значение->__toString();
+        }
+
+        $vendor['quantity'] = null;
+
+        $this->shipper->import($vendor);
+
+    }
+
+
+    public function xmlOffer($inner)
+    {
+
+       // echo 'offer';
+
+        $xml = simplexml_load_string($inner);
+        $vendor['vendor_id'] = $this->vendor_id; // ГОТТИ
+        $vendor['guid'] = $xml->Ид->__toString();
+        //$product['ean'] = $xml->Штрихкод->__toString();
+        $vendor['name'] = $xml->Наименование->__toString();
+        $vendor['quantity'] = (int)$xml->Количество->__toString();
+
+
+        //var_dump($this->shipper->getPrices($this->vendor_id));
+        $prices = array_flip($this->shipper->getPrices($this->vendor_id));
+        $storage = $this->shipper->getStorage($this->vendor_id);
+
+
+        foreach ($xml->Цены->Цена as $price) {
+           /** ждем своего часа */
+
+           if($prices) {
+               if (isset($prices[$price->ИдТипаЦены->__toString()])) {
+                   $vendor[$prices[$price->ИдТипаЦены->__toString()]] = (float)$price->ЦенаЗаЕдиницу->__toString();
+                  // var_dump($vendor['guid'].': '.$prices[$price->ИдТипаЦены->__toString()].' - '.$price->ЦенаЗаЕдиницу->__toString());
+               }
+           } else {
+               $vendor['price'] = (float)$price->ЦенаЗаЕдиницу->__toString();
+           }
+        }
+
+        //if(isset($vendor['price_red'])) var_dump($vendor);
+
+
+        if($storage) {
+            $delivery_array = array();
+            foreach ($xml->Склад as $sklad) {
+
+                // задача: получить наличе по самом ближнему складу
+                $delivery_array[$storage[(string)$sklad['ИдСклада']]] = (int)$sklad['КоличествоНаСкладе'];
+
+            }
+        }
+
+        if(!empty($delivery_array)) {
+
+            ksort($delivery_array);
+
+            //var_dump($storage);
+            //var_dump($delivery_array);
+
+            foreach ($delivery_array as $delivery => $quantity) {
+                if ($quantity > 0) {
+                    $vendor['delivery'] = $delivery;
+                    $vendor['quantity'] = $quantity;
+                    break;
+                }
+            }
+
+            if (!isset($vendor['delivery'])) {
+                $this->null_cnt++;
+                $vendor['quantity'] = 0;
+                $vendor['delivery'] = 100;
+            }
+        } else {
+            $vendor['delivery'] = 0;
+        }
+
+        // для админа
+
+       // $this->sync->setB2bPrice($product['product_id'],$price_6);
+
+
+
+
+        $result_cnt = $this->shipper->import($vendor);
+        //echo  $result_cnt.' ';
+        if(isset($this->{$result_cnt})) $this->{$result_cnt}++;
+
+
+    }
+
+    public function xmlCategory($xml)
+    {
+
+        $xml = simplexml_load_string($xml);
+
+
+        $cat['xml'] = $xml->Ид->__toString();
+        $cat['name'] = $xml->Наименование->__toString();
+
+        $this->shipper->vendor_id = $this->vendor_id;
+        $this->shipper->get_category_id($cat['name'],$cat['xml']);
+
+        //echo $cat['xml'].' - '.$cat['name'];
+
+    }
+}
